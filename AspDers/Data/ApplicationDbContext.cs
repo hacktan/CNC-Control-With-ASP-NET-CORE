c51716e464a0f3a421273f091ecf4bfac3221764
Haktan <haktankocyigit@gmail.com>
2021-05-23 23:19:29 +0300

    bakimtipi

--- AspDers/Data/ApplicationDbContext.cs
+++ AspDers/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using AspDers.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -12,5 +13,6 @@ namespace AspDers.Data
             : base(options)
         {
         }
+        public DbSet<BakimTipi> BakimTipi { get; set; }
     }
 }
